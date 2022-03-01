cf1343ada480270246ad4c3d1e1d22d719173a1c
Milad Nekofar <milad@nekofar.com>
2022-03-01 07:28:42 +0330

    build: add gradle changelog plugin to retrieve changelog

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,6 +9,8 @@ plugins {
     id("org.jetbrains.kotlin.jvm") version "1.6.10"
     // Gradle IntelliJ Plugin
     id("org.jetbrains.intellij") version "1.3.1"
+    // Gradle Changelog Plugin
+    id("org.jetbrains.changelog") version "1.3.1"
 }
 
 group = properties("pluginGroup")
@@ -34,6 +36,12 @@ intellij {
     plugins.set(properties("platformPlugins").split(',').map(String::trim).filter(String::isNotEmpty))
 }
 
+// Configure Gradle Changelog Plugin - read more: https://github.com/JetBrains/gradle-changelog-plugin
+changelog {
+    version.set(properties("pluginVersion"))
+    groups.set(emptyList())
+}
+
 tasks {
     // Set the JVM compatibility versions
     properties("javaVersion").let {
@@ -55,9 +63,12 @@ tasks {
         sinceBuild.set(properties("pluginSinceBuild"))
         untilBuild.set(properties("pluginUntilBuild"))
 
-        changeNotes.set("""
-            Add change notes here.<br>
-            <em>most HTML tags may be used</em>        """.trimIndent())
+        // Get the latest available change notes from the changelog file
+        changeNotes.set(provider {
+            changelog.run {
+                getOrNull(properties("pluginVersion")) ?: getLatest()
+            }.toHTML()
+        })
     }
 }
 
