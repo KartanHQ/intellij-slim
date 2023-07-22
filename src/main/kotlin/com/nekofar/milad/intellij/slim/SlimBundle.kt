41decde329317d5a295579895c123e992ea05aed
Milad Nekofar <milad@nekofar.com>
2023-07-22 15:18:20 +0330

    refactor: update bundle file for code cleanliness
    Updated the code in SlimBundle ktl and properties files. Removed redundant @NonNls annotation, rearranged it for clarity in SlimBundle.kt, and also added a newline at the end of SlimBundle.properties for better code formatting. Improves readability and adherence to code style guidelines.

--- src/main/kotlin/com/nekofar/milad/intellij/slim/SlimBundle.kt
+++ src/main/kotlin/com/nekofar/milad/intellij/slim/SlimBundle.kt
@@ -1,14 +1,13 @@
 package com.nekofar.milad.intellij.slim
 
 import com.intellij.DynamicBundle
-import org.jetbrains.annotations.NonNls
 import org.jetbrains.annotations.PropertyKey
+import org.jetbrains.annotations.NonNls
 
 @NonNls
 private const val BUNDLE = "messages.SlimBundle"
 
 object SlimBundle : DynamicBundle(BUNDLE) {
     @JvmStatic
-    fun message(@NonNls @PropertyKey(resourceBundle = BUNDLE) key: String, vararg params: Any) =
-        getMessage(key, *params)
+    fun message(@PropertyKey(resourceBundle = BUNDLE) key: String, vararg params: Any) = getMessage(key, *params)
 }
